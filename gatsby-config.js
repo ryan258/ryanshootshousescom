e79bd695a18f98b19aca779bccc27db89459a25b
Ryan Johnson <ryan@258webco.com>
2019-08-25 14:50:02 -0500

    things

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/ryanshootshousescom",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Shoots Houses`,
+    description: `Better media, quicker sales!`,
+    author: `@ryanleej`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
